71ab3e5c87985e2a11986d7adec3c8a16fe5d48d
KevinDelcroix <122818638+KevinDelcroix@users.noreply.github.com>
2025-04-16 15:46:25 +0200

    Update script.js

--- FarmoBot/script.js
+++ FarmoBot/script.js
@@ -1,18 +1,3 @@
-function showDownloadPopup() {
-    // Affiche le pop-up
-    document.getElementById("download-popup").style.display = "block";
-
-    // Joue le son
-    var sound = document.getElementById("error-sound");
-    sound.play();
-}
-
-function closePopup() {
-    // Cache le pop-up
-    document.getElementById("download-popup").style.display = "none";
-}
-
-
 const mediaBlocks = document.querySelectorAll('.media-hover');
 
   mediaBlocks.forEach(block => {
